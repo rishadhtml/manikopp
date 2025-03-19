3ad7254786994fe33991c2750f3c2b4bc1d4adcd
rishadhtml <shaaxaal@gmail.com>
2025-03-19 22:58:57 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "file_upload");
-if (!$conn) die("Connection failed: " . mysqli_connect_error());
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Connect to PostgreSQL
+$conn = pg_connect("host={$_ENV['DB_HOST']} port={$_ENV['DB_PORT']} dbname={$_ENV['DB_NAME']} user={$_ENV['DB_USER']} password={$_ENV['DB_PASS']}");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
 ?>
